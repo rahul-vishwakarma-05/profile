516b3f27c9e51c24e5cde617574a5dba6a96b5fd
rahul-vishwakarma-05 <116077468+rahul-vishwakarma-05@users.noreply.github.com>
2023-02-15 20:48:10 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,165 @@
+const add_w = document.querySelector('h3 > ion-icon');
+const profile_t = document.querySelector('.profile_two');
+const withdrawal = document.querySelector('.withdrawal');
+const btn = document.querySelector('.btn > button');
+const swip = document.querySelector('.switch');
+const withd = document.querySelector('.with');
+const check = document.querySelector(".checkbox");
+const outerdep = document.querySelector('.deposite');
+const close = document.querySelector('.withdrawal > ion-icon');
+const withdrawals = document.querySelector('.withdrawals');
+
+//---------------------------------------------------- close button for popup --------------------------------------------------------------------- 
+close.addEventListener('click', () => {
+    profile_t.style.filter = " blur(0)";
+    withdrawal.style.transform = "translateX(-130%)";
+    outerdep.style.transform = "translate(130%)";
+    withdrawals.style.transform = "translateX(-130%)";
+    check.checked = false;
+    withd.style.transform = "translateX(0)";
+});
+
+
+// ----------------------------------------------  withdrawal popup -----------------------------------------   -----------------------------------------------------
+add_w.addEventListener("click", () => {
+    profile_t.style.filter = " blur(10px)";
+    withdrawal.style.transform = "translateX(0)";
+    withdrawals.style.transform = "translateX(0)";
+});
+
+
+
+// ------------------------------------------------- withdrawal btn -------
+
+btn.addEventListener('click', () => {
+    alert("successfully submited")
+});
+
+// ---------------------------------------------------switch system ----------------------------------------------------
+
+swip.addEventListener('click', () => {
+    if (check.checked == true) {
+        outerdep.style.transform = "translate(0)";
+        withd.style.transform = "translate(130%)";
+    } else {
+        outerdep.style.transform = "translate(130%)";
+        withd.style.transform = "translate(0)";
+    }
+});
+
+
+
+// ------------------------------------------------ js for users profile details ---------------------------------------------------------------------
+
+const pro_edit_id1 = document.querySelector('#one');
+// const pro_edit_icon_id1 = document.querySelector('#one');
+
+const pro_edit_id2 = document.querySelector('#two');
+// const pro_edit_icon_id2 = document.querySelector('#two');
+
+const pro_edit_id3 = document.querySelector('#three');
+// const pro_edit_icon_id3 = document.querySelector('#three');
+
+const pro_edit_id4 = document.querySelector('#four');
+// const pro_edit_icon_id4 = document.querySelector('#four');
+
+const pro_same_clss = document.querySelectorAll('.same');
+let pro_icon_sams = document.querySelectorAll('.sams');
+
+pro_same_clss.forEach(element => {
+    element.disabled = true;
+});
+pro_icon_sams.forEach(element => {
+    element.addEventListener('click', () => {
+        pro_same_clss.forEach(i => {
+            if (i.id == element.id) {
+                i.disabled = false;
+            } else {
+                i.disabled = true;
+            }
+        });
+    });
+});
+
+
+// ------------------------------------------- users settngs sliders -------------------------------------------------------------------------------------------------------
+
+
+const user_icon = document.querySelector('.fa-user');
+const bank_icon = document.querySelector('.fa-bank');
+const upi_icon = document.querySelector('.upi');
+const phone_icon = document.querySelector('.fa-phone');
+
+const pro_det = document.querySelector('.pro_det');
+const bank_det = document.querySelector('.bank_det');
+const upi_main_page = document.querySelector('.upi_page');
+const contact_page = document.querySelector('.contact_page');
+
+
+user_icon.addEventListener('click', () => {
+    pro_det.style.cssText = ` transform:scaleY(1);height:50%;`;
+    bank_det.style.cssText = `transform:scaleY(0);height:0;`;
+    upi_main_page.style.cssText = `transform:scaleY(0);height:0;`;
+    contact_page.style.cssText = `transform:scaleY(0);height:0;`;
+
+});
+
+
+bank_icon.addEventListener('click', () => {
+    pro_det.style.cssText = ` transform:scaleY(0);height:0;`;
+    bank_det.style.cssText = `transform:scaleY(1);height:48%; margin:0`;
+    upi_main_page.style.cssText = `transform:scaleY(0);height:0;`;
+    contact_page.style.cssText = `transform:scaleY(0);height:0;`;
+
+});
+
+
+upi_icon.addEventListener('click', () => {
+    pro_det.style.cssText = ` transform:scaleY(0);height:0;`;
+    bank_det.style.cssText = `transform:scaleY(0);height:0;margin:0`;
+    upi_main_page.style.cssText = `transform:scaleY(1);height:48%;`;
+    contact_page.style.cssText = `transform:scaleY(0);height:0;`;
+
+});
+
+
+phone_icon.addEventListener('click', () => {
+    pro_det.style.cssText = ` transform:scaleY(0);height:0;`;
+    bank_det.style.cssText = `transform:scaleY(0);height:0;`;
+    upi_main_page.style.cssText = `transform:scaleY(0);height:0;`;
+    contact_page.style.cssText = `transform:scaleY(1);height:48%;`;
+
+});
+
+// --------------------------------------------- writting js for the upi id -------------------------------------------------------------------------------------------------------------------------------
+const upi_add_btn = document.querySelector('.s_upi > span > ion-icon')
+// const upi_main_page = document.querySelector('.upi_page');
+
+const addNewpage = () => {
+    const upi_page = document.createElement('div');
+    upi_page.classList.add('upis');
+    const htmlData = `<input type="text" placeholder="add your upi id">`;
+    upi_page.insertAdjacentHTML('afterbegin', htmlData);
+    upi_main_page.appendChild(upi_page);
+}
+
+upi_add_btn.addEventListener('click', () => {
+    addNewpage();
+});
+
+
+// -------------------------------------------------- writting the js for settings ----------------------------------------------------------------------------------------------------------------
+
+const setting_btn   = document.querySelector('.profile_two > h4 > ion-icon');
+const setting_body  = document.querySelector('.setting');
+const setting_arrow = document.querySelector('.setting > h3 > ion-icon');
+
+setting_btn.addEventListener('click' , ()=>{
+    profile_t.style.transform = "translateX(130%)"
+    setting_body.style.transform = "translateX(0%)"
+});
+
+setting_arrow.addEventListener('click',()=>{
+    profile_t.style.transform = "translateX(0)"
+    setting_body.style.transform = "translateX(130%)"
+});
